25a3f6b938d9153dc5c45afbd2c9768382ef8058
bryan31 <weenyc31@163.com>
2020-10-02 00:00:48 +0800

    enhancement: #I1X35S 在logback中AsyncAppender异步appender不生效

---
+++ tlog-core/src/main/java/com/yomahub/tlog/core/enhance/logback/async/AspectLogbackAsyncAppender.java
@@ -0,0 +1,44 @@
+package com.yomahub.tlog.core.enhance.logback.async;
+
+import ch.qos.logback.classic.AsyncAppender;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.classic.spi.LoggingEvent;
+import cn.hutool.core.util.ReflectUtil;
+import cn.hutool.core.util.StrUtil;
+import com.yomahub.tlog.core.context.AspectLogContext;
+import org.apache.commons.lang3.StringUtils;
+
+import java.lang.reflect.Field;
+
+/**
+ * Logback的异步日志增强appender
+ * @author Bryan.Zhang
+ * @since 2020/9/30
+ */
+public class AspectLogbackAsyncAppender extends AsyncAppender {
+
+    private Field field;
+
+    @Override
+    protected void append(ILoggingEvent eventObject) {
+        if(eventObject instanceof LoggingEvent){
+            LoggingEvent loggingEvent = (LoggingEvent)eventObject;
+            String resultLog;
+
+            if(StringUtils.isNotBlank(AspectLogContext.getLogValue())){
+                resultLog = StrUtil.format("{} {}", AspectLogContext.getLogValue(),loggingEvent.getMessage());
+                if(field == null){
+                    field = ReflectUtil.getField(LoggingEvent.class,"formattedMessage");
+                    field.setAccessible(true);
+
+                    try {
+                        field.set(loggingEvent,resultLog);
+                    } catch (IllegalAccessException e) {
+                    }
+                }
+            }
+            eventObject = loggingEvent;
+        }
+        super.append(eventObject);
+    }
+}
