a6dd737c788b30575250b6c58353595cf7689738
bryan31 <weenyc31@163.com>
2020-10-02 00:01:16 +0800

    enhancement: #I1XD4U 支持log4j中的异步日志appender方式

---
+++ tlog-core/src/main/java/com/yomahub/tlog/core/enhance/log4j/async/AspectLog4jAsyncAppender.java
@@ -0,0 +1,35 @@
+package com.yomahub.tlog.core.enhance.log4j.async;
+
+import cn.hutool.core.util.ReflectUtil;
+import cn.hutool.core.util.StrUtil;
+import com.yomahub.tlog.core.context.AspectLogContext;
+import org.apache.log4j.AsyncAppender;
+import org.apache.log4j.spi.LoggingEvent;
+
+import java.lang.reflect.Field;
+
+/**
+ * Log4j的异步日志增强appender
+ * @author Bryan.Zhang
+ * @since 2020/9/30
+ */
+public class AspectLog4jAsyncAppender extends AsyncAppender {
+
+    private Field field;
+
+    public void doAppend(LoggingEvent event) {
+        String resultLog = StrUtil.format("{} {}", AspectLogContext.getLogValue(),event.getMessage());
+
+        if(field == null){
+            field = ReflectUtil.getField(LoggingEvent.class,"message");
+            field.setAccessible(true);
+        }
+
+        try {
+            field.set(event,resultLog);
+        } catch (IllegalAccessException e) {
+        }
+
+        super.doAppend(event);
+    }
+}
