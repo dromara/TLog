3d0172f774cbdd833b54bbb3a4e8f778d36f90c9
bryan31 <weenyc31@163.com>
2020-10-19 20:03:53 +0800

    temp submit

--- tlog-core/src/main/java/com/yomahub/tlog/core/enhance/bytes/logback/LogbackBytesSyncEnhance.java
+++ tlog-core/src/main/java/com/yomahub/tlog/core/enhance/bytes/logback/LogbackBytesSyncEnhance.java
@@ -1,7 +1,16 @@
 package com.yomahub.tlog.core.enhance.bytes.logback;
 
 import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.classic.spi.LoggingEvent;
+import ch.qos.logback.core.UnsynchronizedAppenderBase;
+import ch.qos.logback.core.spi.FilterReply;
+import ch.qos.logback.core.status.WarnStatus;
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.ReflectUtil;
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import com.yomahub.tlog.core.context.AspectLogContext;
+
+import java.lang.reflect.Field;
 
 /**
  * Logback的字节增强类
@@ -10,12 +19,53 @@ import com.yomahub.tlog.core.context.AspectLogContext;
  */
 public class LogbackBytesSyncEnhance {
 
-    public static String enhance(ILoggingEvent event){
-        String logValue = AspectLogContext.getLogValue();
-        if(logValue != null){
-            return logValue + " " + event.getFormattedMessage();
-        }else{
-            return event.getFormattedMessage();
+    private static Field guardField;
+
+    private static Field startedField;
+
+    public static void enhance(ILoggingEvent event, UnsynchronizedAppenderBase thisObj){
+
+        if (guardField == null){
+            guardField = ReflectUtil.getField(LoggingEvent.class,"guardField");
+            guardField.setAccessible(true);
         }
+
+        ThreadLocal<Boolean> guardFieldValue = null;
+        try{
+            guardFieldValue = (ThreadLocal<Boolean>)guardField.get(thisObj);
+        }catch (Exception e){
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+        if (Boolean.TRUE.equals(guardFieldValue.get())) {
+            return;
+        }
+
+        try {
+            guardFieldValue.set(Boolean.TRUE);
+
+            if (!this.started) {
+                if (statusRepeatCount++ < ALLOWED_REPEATS) {
+                    addStatus(new WarnStatus("Attempted to append to non started appender [" + name + "].", this));
+                }
+                return;
+            }
+
+            if (getFilterChainDecision(eventObject) == FilterReply.DENY) {
+                return;
+            }
+
+            // ok, we now invoke derived class' implementation of append
+            this.append(eventObject);
+
+        } catch (Exception e) {
+            if (exceptionCount++ < ALLOWED_REPEATS) {
+                addError("Appender [" + name + "] failed to append.", e);
+            }
+        } finally {
+            guard.set(Boolean.FALSE);
+        }
+
     }
 }
