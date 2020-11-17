4db36df2c2b565f85e4e98294997e7517b66124e
bryan31 <weenyc31@163.com>
2020-11-17 17:14:04 +0800

    Revert "finish issues/I24YCX"
    This reverts commit 1117f6a8

--- tlog-dubbo/src/main/java/com/yomahub/tlog/dubbo/filter/TLogDubboFilter.java
+++ tlog-dubbo/src/main/java/com/yomahub/tlog/dubbo/filter/TLogDubboFilter.java
@@ -14,101 +14,95 @@ import org.apache.dubbo.rpc.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
-import utils.Times;
 
 /**
  * dubbo的调用拦截器
- *
  * @author Bryan.Zhang
  * @since 2020/9/11
  */
-@Activate(group = {CommonConstants.PROVIDER, CommonConstants.CONSUMER}, order = -10000)
+@Activate(group = {CommonConstants.PROVIDER, CommonConstants.CONSUMER},order = -10000)
 public class TLogDubboFilter implements Filter {
 
-    private static final Logger log = LoggerFactory.getLogger(TLogDubboFilter.class);
+    private Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
-        final Result[] result = new Result[1];
+        Result result;
+        String side = invoker.getUrl().getParameter(CommonConstants.SIDE_KEY);
 
-        Times.timeConsuming(true, "TLogDubboFilter", log, new Times.Task() {
-            @Override
-            public void execute() {
-                String side = invoker.getUrl().getParameter(CommonConstants.SIDE_KEY);
+        if(side.equals(CommonConstants.PROVIDER_SIDE)){
+            String preIvkApp = invocation.getAttachment(TLogConstants.PRE_IVK_APP_KEY);
+            String preIp = invocation.getAttachment(TLogConstants.PRE_IP_KEY);
+            String traceId = invocation.getAttachment(TLogConstants.TLOG_TRACE_KEY);
+            String spanId = invocation.getAttachment(TLogConstants.TLOG_SPANID_KEY);
 
-                if (side.equals(CommonConstants.PROVIDER_SIDE)) {
-                    String preIvkApp = invocation.getAttachment(TLogConstants.PRE_IVK_APP_KEY);
-                    String preIp = invocation.getAttachment(TLogConstants.PRE_IP_KEY);
-                    String traceId = invocation.getAttachment(TLogConstants.TLOG_TRACE_KEY);
-                    String spanId = invocation.getAttachment(TLogConstants.TLOG_SPANID_KEY);
+            if(StringUtils.isBlank(preIvkApp)){
+                preIvkApp = TLogConstants.UNKNOWN;
+            }
+            if(StringUtils.isBlank(preIp)){
+                preIp = TLogConstants.UNKNOWN;
+            }
 
-                    if (StringUtils.isBlank(preIvkApp)) {
-                        preIvkApp = TLogConstants.UNKNOWN;
-                    }
-                    if (StringUtils.isBlank(preIp)) {
-                        preIp = TLogConstants.UNKNOWN;
-                    }
+            TLogContext.putPreIvkApp(preIvkApp);
+            TLogContext.putPreIp(preIp);
 
-                    TLogContext.putPreIvkApp(preIvkApp);
-                    TLogContext.putPreIp(preIp);
+            //如果从隐式传参里没有获取到，则重新生成一个traceId
+            if(StringUtils.isBlank(traceId)){
+                traceId = UniqueIdGenerator.generateStringId();
+                log.debug("[TLOG]可能上一个节点[{}]没有没有正确传递traceId,重新生成traceId[{}]",preIvkApp,traceId);
+            }
 
-                    //如果从隐式传参里没有获取到，则重新生成一个traceId
-                    if (StringUtils.isBlank(traceId)) {
-                        traceId = UniqueIdGenerator.generateStringId();
-                        log.debug("[TLOG]可能上一个节点[{}]没有没有正确传递traceId,重新生成traceId[{}]", preIvkApp, traceId);
-                    }
+            //往TLog上下文里放当前获取到的spanId，如果spanId为空，会放入初始值
+            TLogContext.putSpanId(spanId);
 
-                    //往TLog上下文里放当前获取到的spanId，如果spanId为空，会放入初始值
-                    TLogContext.putSpanId(spanId);
+            //往TLog上下文里放一个当前的traceId
+            TLogContext.putTraceId(traceId);
 
-                    //往TLog上下文里放一个当前的traceId
-                    TLogContext.putTraceId(traceId);
+            //生成日志标签
+            String tlogLabel = TLogLabelGenerator.generateTLogLabel(preIvkApp,preIp,traceId,TLogContext.getSpanId());
 
-                    //生成日志标签
-                    String tlogLabel = TLogLabelGenerator.generateTLogLabel(preIvkApp, preIp, traceId, TLogContext.getSpanId());
+            //往日志切面器里放一个日志前缀
+            AspectLogContext.putLogValue(tlogLabel);
 
-                    //往日志切面器里放一个日志前缀
-                    AspectLogContext.putLogValue(tlogLabel);
+            //如果有MDC，则往MDC中放入日志标签
+            if(TLogContext.hasTLogMDC()){
+                MDC.put(TLogConstants.MDC_KEY, tlogLabel);
+            }
 
-                    //如果有MDC，则往MDC中放入日志标签
-                    if (TLogContext.hasTLogMDC()) {
-                        MDC.put(TLogConstants.MDC_KEY, tlogLabel);
-                    }
-
-                    try {
-                        //调用dubbo
-                        result[0] = invoker.invoke(invocation);
-                    } finally {
-                        //移除ThreadLocal里的数据
-                        TLogContext.removePreIvkApp();
-                        TLogContext.removePreIp();
-                        TLogContext.removeTraceId();
-                        TLogContext.removeSpanId();
-                        AspectLogContext.remove();
-                        if (TLogContext.hasTLogMDC()) {
-                            MDC.remove(TLogConstants.MDC_KEY);
-                        }
-                    }
-                } else if (side.equals(CommonConstants.CONSUMER_SIDE)) {
-                    String traceId = TLogContext.getTraceId();
-
-                    if (StringUtils.isNotBlank(traceId)) {
-                        String appName = invoker.getUrl().getParameter(CommonConstants.APPLICATION_KEY);
-                        String ip = NetUtil.getLocalhostStr();
-
-                        RpcContext.getContext().setAttachment(TLogConstants.TLOG_TRACE_KEY, traceId);
-                        RpcContext.getContext().setAttachment(TLogConstants.PRE_IVK_APP_KEY, appName);
-                        RpcContext.getContext().setAttachment(TLogConstants.PRE_IP_KEY, ip);
-                        RpcContext.getContext().setAttachment(TLogConstants.TLOG_SPANID_KEY, SpanIdGenerator.generateNextSpanId());
-                    } else {
-                        log.warn("[TLOG]本地threadLocal变量没有正确传递traceId,本次调用不传递traceId");
-                    }
-                    result[0] = invoker.invoke(invocation);
-                } else {
-                    result[0] = null;
+            try{
+                //调用dubbo
+                result = invoker.invoke(invocation);
+            }finally {
+                //移除ThreadLocal里的数据
+                TLogContext.removePreIvkApp();
+                TLogContext.removePreIp();
+                TLogContext.removeTraceId();
+                TLogContext.removeSpanId();
+                AspectLogContext.remove();
+                if(TLogContext.hasTLogMDC()){
+                    MDC.remove(TLogConstants.MDC_KEY);
                 }
             }
-        });
-        return result[0];
+
+            return result;
+        }else if(side.equals(CommonConstants.CONSUMER_SIDE)){
+            String traceId = TLogContext.getTraceId();
+
+            if(StringUtils.isNotBlank(traceId)){
+                String appName = invoker.getUrl().getParameter(CommonConstants.APPLICATION_KEY);
+                String ip = NetUtil.getLocalhostStr();
+
+                RpcContext.getContext().setAttachment(TLogConstants.TLOG_TRACE_KEY,traceId);
+                RpcContext.getContext().setAttachment(TLogConstants.PRE_IVK_APP_KEY,appName);
+                RpcContext.getContext().setAttachment(TLogConstants.PRE_IP_KEY,ip);
+                RpcContext.getContext().setAttachment(TLogConstants.TLOG_SPANID_KEY, SpanIdGenerator.generateNextSpanId());
+            }else{
+                log.warn("[TLOG]本地threadLocal变量没有正确传递traceId,本次调用不传递traceId");
+            }
+            result = invoker.invoke(invocation);
+        }else{
+            result = null;
+        }
+        return result;
     }
 }
--- tlog-dubbox/src/main/java/com/yomahub/tlog/dubbox/filter/TLogDubboxFilter.java
+++ tlog-dubbox/src/main/java/com/yomahub/tlog/dubbox/filter/TLogDubboxFilter.java
@@ -14,104 +14,95 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
-import utils.Times;
 
 /**
  * dubbox的调用拦截器
- *
  * @author Bryan.Zhang
  * @since 2020/9/11
  */
-@Activate(group = {Constants.PROVIDER, Constants.CONSUMER}, order = -10000)
+@Activate(group = {Constants.PROVIDER, Constants.CONSUMER},order = -10000)
 public class TLogDubboxFilter implements Filter {
 
     private static final Logger log = LoggerFactory.getLogger(TLogDubboxFilter.class);
 
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
-        final Result[] result = new Result[1];
+        Result result;
+        String side = invoker.getUrl().getParameter(Constants.SIDE_KEY);
 
-        Times.timeConsuming(true, "TLogDubboxFilter", log, new Times.Task() {
+        if(side.equals(Constants.PROVIDER_SIDE)){
+            String preIvkApp = invocation.getAttachment(TLogConstants.PRE_IVK_APP_KEY);
+            String preIp = invocation.getAttachment(TLogConstants.PRE_IP_KEY);
+            String traceId = invocation.getAttachment(TLogConstants.TLOG_TRACE_KEY);
+            String spanId = invocation.getAttachment(TLogConstants.TLOG_SPANID_KEY);
 
-            @Override
-            public void execute() {
+            if(StringUtils.isBlank(preIvkApp)){
+                preIvkApp = TLogConstants.UNKNOWN;
+            }
+            if(StringUtils.isBlank(preIp)){
+                preIp = TLogConstants.UNKNOWN;
+            }
 
-                String side = invoker.getUrl().getParameter(Constants.SIDE_KEY);
+            TLogContext.putPreIvkApp(preIvkApp);
+            TLogContext.putPreIp(preIp);
 
-                if (side.equals(Constants.PROVIDER_SIDE)) {
-                    String preIvkApp = invocation.getAttachment(TLogConstants.PRE_IVK_APP_KEY);
-                    String preIp = invocation.getAttachment(TLogConstants.PRE_IP_KEY);
-                    String traceId = invocation.getAttachment(TLogConstants.TLOG_TRACE_KEY);
-                    String spanId = invocation.getAttachment(TLogConstants.TLOG_SPANID_KEY);
+            //如果从隐式传参里没有获取到，则重新生成一个traceId
+            if(StringUtils.isBlank(traceId)){
+                traceId = UniqueIdGenerator.generateStringId();
+                log.debug("[TLOG]可能上一个节点[{}]没有没有正确传递traceId,重新生成traceId[{}]",preIvkApp,traceId);
+            }
 
-                    if (StringUtils.isBlank(preIvkApp)) {
-                        preIvkApp = TLogConstants.UNKNOWN;
-                    }
-                    if (StringUtils.isBlank(preIp)) {
-                        preIp = TLogConstants.UNKNOWN;
-                    }
+            //往TLog上下文里放当前获取到的spanId，如果spanId为空，会放入初始值
+            TLogContext.putSpanId(spanId);
 
-                    TLogContext.putPreIvkApp(preIvkApp);
-                    TLogContext.putPreIp(preIp);
+            //往TLog上下文里放一个当前的traceId
+            TLogContext.putTraceId(traceId);
 
-                    //如果从隐式传参里没有获取到，则重新生成一个traceId
-                    if (StringUtils.isBlank(traceId)) {
-                        traceId = UniqueIdGenerator.generateStringId();
-                        log.debug("[TLOG]可能上一个节点[{}]没有没有正确传递traceId,重新生成traceId[{}]", preIvkApp, traceId);
-                    }
+            //生成日志标签
+            String tlogLabel = TLogLabelGenerator.generateTLogLabel(preIvkApp,preIp,traceId,TLogContext.getSpanId());
 
-                    //往TLog上下文里放当前获取到的spanId，如果spanId为空，会放入初始值
-                    TLogContext.putSpanId(spanId);
+            //往日志切面器里放一个日志前缀
+            AspectLogContext.putLogValue(tlogLabel);
 
-                    //往TLog上下文里放一个当前的traceId
-                    TLogContext.putTraceId(traceId);
+            //如果有MDC，则往MDC中放入日志标签
+            if(TLogContext.hasTLogMDC()){
+                MDC.put(TLogConstants.MDC_KEY, tlogLabel);
+            }
 
-                    //生成日志标签
-                    String tlogLabel = TLogLabelGenerator.generateTLogLabel(preIvkApp, preIp, traceId, TLogContext.getSpanId());
-
-                    //往日志切面器里放一个日志前缀
-                    AspectLogContext.putLogValue(tlogLabel);
-
-                    //如果有MDC，则往MDC中放入日志标签
-                    if (TLogContext.hasTLogMDC()) {
-                        MDC.put(TLogConstants.MDC_KEY, tlogLabel);
-                    }
-
-                    try {
-                        //调用dubbo
-                        result[0] = invoker.invoke(invocation);
-                    } finally {
-                        //移除ThreadLocal里的数据
-                        TLogContext.removePreIvkApp();
-                        TLogContext.removePreIp();
-                        TLogContext.removeTraceId();
-                        TLogContext.removeSpanId();
-                        AspectLogContext.remove();
-                        if (TLogContext.hasTLogMDC()) {
-                            MDC.remove(TLogConstants.MDC_KEY);
-                        }
-                    }
-                } else if (side.equals(Constants.CONSUMER_SIDE)) {
-                    String traceId = TLogContext.getTraceId();
-
-                    if (StringUtils.isNotBlank(traceId)) {
-                        String appName = invoker.getUrl().getParameter(Constants.APPLICATION_KEY);
-                        String ip = NetUtil.getLocalhostStr();
-
-                        RpcContext.getContext().setAttachment(TLogConstants.TLOG_TRACE_KEY, traceId);
-                        RpcContext.getContext().setAttachment(TLogConstants.PRE_IVK_APP_KEY, appName);
-                        RpcContext.getContext().setAttachment(TLogConstants.PRE_IP_KEY, ip);
-                        RpcContext.getContext().setAttachment(TLogConstants.TLOG_SPANID_KEY, SpanIdGenerator.generateNextSpanId());
-                    } else {
-                        log.warn("[TLOG]本地threadLocal变量没有正确传递traceId,本次调用不传递traceId");
-                    }
-                    result[0] = invoker.invoke(invocation);
-                } else {
-                    result[0] = null;
+            try{
+                //调用dubbo
+                result = invoker.invoke(invocation);
+            }finally {
+                //移除ThreadLocal里的数据
+                TLogContext.removePreIvkApp();
+                TLogContext.removePreIp();
+                TLogContext.removeTraceId();
+                TLogContext.removeSpanId();
+                AspectLogContext.remove();
+                if(TLogContext.hasTLogMDC()){
+                    MDC.remove(TLogConstants.MDC_KEY);
                 }
             }
-        });
 
-        return result[0];
+            return result;
+        }else if(side.equals(Constants.CONSUMER_SIDE)){
+            String traceId = TLogContext.getTraceId();
+
+            if(StringUtils.isNotBlank(traceId)){
+                String appName = invoker.getUrl().getParameter(Constants.APPLICATION_KEY);
+                String ip = NetUtil.getLocalhostStr();
+
+                RpcContext.getContext().setAttachment(TLogConstants.TLOG_TRACE_KEY,traceId);
+                RpcContext.getContext().setAttachment(TLogConstants.PRE_IVK_APP_KEY,appName);
+                RpcContext.getContext().setAttachment(TLogConstants.PRE_IP_KEY,ip);
+                RpcContext.getContext().setAttachment(TLogConstants.TLOG_SPANID_KEY,SpanIdGenerator.generateNextSpanId());
+            }else{
+                log.warn("[TLOG]本地threadLocal变量没有正确传递traceId,本次调用不传递traceId");
+            }
+            result = invoker.invoke(invocation);
+        }else{
+            result = null;
+        }
+        return result;
     }
 }
