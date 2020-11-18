50a6195ff53762b60f232a29b3a18442721a3aeb
bryan31 <weenyc31@163.com>
2020-11-18 17:30:38 +0800

    feature: #I24YCX 针对于dubbo和feign提供调用参数，调用时长的自动打印

---
+++ tlog-common/src/main/java/com/yomahub/tlog/spring/TLogPropertyInit.java
@@ -0,0 +1,34 @@
+package com.yomahub.tlog.spring;
+
+import com.yomahub.tlog.context.TLogContext;
+import com.yomahub.tlog.context.TLogLabelGenerator;
+import org.springframework.beans.factory.InitializingBean;
+
+public class TLogPropertyInit implements InitializingBean {
+
+    private String pattern;
+
+    private boolean enableInvokeTimePrint;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        TLogLabelGenerator.setLabelPattern(pattern);
+        TLogContext.setEnableInvokeTimePrint(enableInvokeTimePrint);
+    }
+
+    public String getPattern() {
+        return pattern;
+    }
+
+    public void setPattern(String pattern) {
+        this.pattern = pattern;
+    }
+
+    public boolean getEnableInvokeTimePrint() {
+        return enableInvokeTimePrint;
+    }
+
+    public void setEnableInvokeTimePrint(boolean enableInvokeTimePrint) {
+        this.enableInvokeTimePrint = enableInvokeTimePrint;
+    }
+}
