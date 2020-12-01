59ccc8a66a275818eeb4a57dfe00caf4298bca83
bryan31 <weenyc31@163.com>
2020-12-01 17:50:37 +0800

    bug: #I274EL 非MDC模式中，定义多个appender有可能会重复显示日志的问题

--- tlog-core/src/main/java/com/yomahub/tlog/core/enhance/logback/async/AspectLogbackAsyncAppender.java
+++ tlog-core/src/main/java/com/yomahub/tlog/core/enhance/logback/async/AspectLogbackAsyncAppender.java
@@ -3,12 +3,16 @@ package com.yomahub.tlog.core.enhance.logback.async;
 import ch.qos.logback.classic.AsyncAppender;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.classic.spi.LoggingEvent;
+import cn.hutool.cache.Cache;
+import cn.hutool.cache.CacheUtil;
+import cn.hutool.core.collection.ConcurrentHashSet;
 import cn.hutool.core.util.ReflectUtil;
 import cn.hutool.core.util.StrUtil;
 import com.yomahub.tlog.core.context.AspectLogContext;
 import org.apache.commons.lang3.StringUtils;
 
 import java.lang.reflect.Field;
+import java.util.HashSet;
 
 /**
  * Logback的异步日志增强appender
@@ -20,25 +24,31 @@ public class AspectLogbackAsyncAppender extends AsyncAppender {
 
     private Field field;
 
+    private static final Cache<LoggingEvent, Integer> lruCache = CacheUtil.newLRUCache(2048);
+
     @Override
     protected void append(ILoggingEvent eventObject) {
         if(eventObject instanceof LoggingEvent){
             LoggingEvent loggingEvent = (LoggingEvent)eventObject;
-            String resultLog;
 
-            if(StringUtils.isNotBlank(AspectLogContext.getLogValue())){
-                resultLog = StrUtil.format("{} {}", AspectLogContext.getLogValue(),loggingEvent.getFormattedMessage());
-                if(field == null){
-                    field = ReflectUtil.getField(LoggingEvent.class,"formattedMessage");
-                    field.setAccessible(true);
-                }
+            if (!lruCache.containsKey(loggingEvent)){
+                String resultLog;
 
-                try {
-                    field.set(loggingEvent,resultLog);
-                } catch (IllegalAccessException e) {
+                if(StringUtils.isNotBlank(AspectLogContext.getLogValue())){
+                    resultLog = StrUtil.format("{} {}", AspectLogContext.getLogValue(),loggingEvent.getFormattedMessage());
+                    if(field == null){
+                        field = ReflectUtil.getField(LoggingEvent.class,"formattedMessage");
+                        field.setAccessible(true);
+                    }
+
+                    try {
+                        field.set(loggingEvent,resultLog);
+                    } catch (IllegalAccessException e) {
+                    }
                 }
+                eventObject = loggingEvent;
+                lruCache.put(loggingEvent, Integer.MIN_VALUE);
             }
-            eventObject = loggingEvent;
         }
         super.append(eventObject);
     }
