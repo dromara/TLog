472fcfc273f04dfa9a5ec85f792106b36acc37fc
Naah <nayan3480232@163.com>
2021-04-13 11:56:43 +0800

    adapte soul gateway

---
+++ tlog-all-spring-boot-starter/src/main/java/com/yomahub/tlog/springboot/TLogSoulGatewayAutoConfiguration.java
@@ -0,0 +1,31 @@
+package com.yomahub.tlog.springboot;
+
+import com.yomahub.tlog.webflux.aop.SoulPluginLogAop;
+import com.yomahub.tlog.webflux.filter.TLogWebFluxFilter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+
+/**
+ * @author naah
+ * @since 1.2.4-BETA1
+ */
+@Configuration
+@ConditionalOnClass(name = {"org.dromara.soul.web.handler.SoulWebHandler"})
+public class TLogSoulGatewayAutoConfiguration {
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public TLogWebFluxFilter traceFilter() {
+        return new TLogWebFluxFilter();
+    }
+
+    @Bean
+    public SoulPluginLogAop pluginAop(){
+        return new SoulPluginLogAop();
+    }
+
+}
+
