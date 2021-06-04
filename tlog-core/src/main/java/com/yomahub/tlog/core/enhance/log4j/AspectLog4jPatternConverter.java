7dad02c78c4a2e596ddcb751581b82f085aed4e7
刘亚洲 <987773698@qq.com>
2021-06-04 17:46:27 +0800

    update tlog-core/src/main/java/com/yomahub/tlog/core/enhance/log4j/AspectLog4jPatternConverter.java.

--- tlog-core/src/main/java/com/yomahub/tlog/core/enhance/log4j/AspectLog4jPatternConverter.java
+++ tlog-core/src/main/java/com/yomahub/tlog/core/enhance/log4j/AspectLog4jPatternConverter.java
@@ -15,16 +15,12 @@ import org.apache.log4j.spi.LoggingEvent;
 public class AspectLog4jPatternConverter extends PatternConverter {
     @Override
     protected String convert(LoggingEvent loggingEvent) {
-        //只有在MDC没有设置的情况下才加到message里
-        if(!TLogContext.hasTLogMDC()){
-            String logValue = AspectLogContext.getLogValue();
-            if(StringUtils.isNotBlank(logValue)){
-                return logValue + " " + loggingEvent.getRenderedMessage();
-            }else{
-                return loggingEvent.getRenderedMessage();
-            }
-        }else{
-            return loggingEvent.getRenderedMessage();
+       //只有在MDC没有设置的情况下才加到message里
+       String renderedMessage = loggingEvent.getRenderedMessage();
+        String logValue = AspectLogContext.getLogValue();
+        if (StringUtils.isNotBlank(logValue) && !TLogContext.hasTLogMDC()) {
+            return logValue + " " + renderedMessage;
         }
+        return renderedMessage;
     }
 }
