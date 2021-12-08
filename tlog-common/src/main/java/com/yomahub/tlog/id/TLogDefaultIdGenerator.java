a0f499168bead8971f22d463e0aefaf2a82a9f58
bryan31 <weenyc31@163.com>
2021-12-08 16:30:54 +0800

    enhancement #I4LNF8 切换默认ID生成器为Hutool包的中雪花生成器

--- tlog-common/src/main/java/com/yomahub/tlog/id/TLogDefaultIdGenerator.java
+++ tlog-common/src/main/java/com/yomahub/tlog/id/TLogDefaultIdGenerator.java
@@ -1,10 +1,14 @@
 package com.yomahub.tlog.id;
 
-import com.yomahub.tlog.id.snowflake.UniqueIdGenerator;
+import cn.hutool.core.util.IdUtil;
 
 public class TLogDefaultIdGenerator extends TLogIdGenerator{
     @Override
     public String generateTraceId() {
-        return UniqueIdGenerator.generateStringId();
+        return IdUtil.getSnowflake().nextIdStr();
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
     }
 }
